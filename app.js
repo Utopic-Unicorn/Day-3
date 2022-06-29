fac10ba045959f75d01b313e71c1f36ce6d8a86d
Christian Barrios <99978665+Utopic-Unicorn@users.noreply.github.com>
2022-06-30 04:24:59 +0800

    added search button

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const open = document.querySelector('#open');
 const close = document.querySelector('#close');
 const container= document.querySelector('.container');
+const search = document.querySelector('.search');
+const button = document.querySelector('.btn');
+const input = document.querySelector('.input')
 
-open.addEventListener('click', ()=> container.classList.add('show-nav'));
-close.addEventListener('click', ()=> container.classList.remove('show-nav'));
+open.addEventListener('click', ()=> {container.classList.add('show-nav')
+search.classList.remove('dis')});
+close.addEventListener('click', ()=> {
+    container.classList.remove('show-nav')
+    search.classList.add('dis')
+});
 // window.addEventListener('scroll', () => container.classList.remove('show-nav'))
+button.addEventListener('click', ()=>{
+    search.classList.toggle('active')
+    input.focus()
+})
